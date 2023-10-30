80ee1d1cb5e89bfcfd8dfb28ed3d44699f20eb08
Ana Luiza Mendes <101937119+aluizamendes@users.noreply.github.com>
2023-10-30 11:04:45 -0300

    adiciona rota para pagina de comics

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import Inicio from './pages/Inicio/index.tsx'
 import Favoritos from './pages/Favoritos/index.tsx'
 import Personagem from './pages/Personagem/index.tsx'
 import PaginaBase from './pages/PaginaBase/index.tsx'
+import Comics from './pages/Comics/index.tsx'
 
 
 export default function AppRoutes() {
@@ -13,9 +14,10 @@ export default function AppRoutes() {
         <Route path='/' element={<PaginaBase />}>
           <Route index element={<Inicio />} />
           <Route path='/favoritos' element={<Favoritos />} />
+          <Route path='/comics' element={<Comics />} />
           <Route path='/:id' element={<Personagem />} />
         </Route>
-      </Routes>    
+      </Routes>
     </BrowserRouter>
   )
 }
