3ab9d982f93455e7fad2d43dadc45d385c5934fc
Ana Luiza Mendes <101937119+aluizamendes@users.noreply.github.com>
2023-12-18 21:22:54 -0300

    ajuste na altura e cor e altura do main

--- src/pages/PaginaBase/index.tsx
+++ src/pages/PaginaBase/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Cabecalho from "../../components/Cabecalho";
+import styles from "./PaginaBase.module.scss"
 
 export default function PaginaBase() {
     return (
         <>
             <Cabecalho />
-            <main>
+            <main className={styles.main}>
                 <Outlet />
             </main>
         </>
